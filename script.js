27427fa380d527175512e367088cb784a648e0d5
LavkushMaurya <LavkushMaurya@example.com>
2024-03-09 09:29:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function clearAll() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result.toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        alert('Invalid expression!');
+        clearDisplay();
+    }
+}
